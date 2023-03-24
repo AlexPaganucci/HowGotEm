bff4e766d17b77d3cd42e1642f8b551feaa7d96c
AlexPaganucci <114147785+AlexPaganucci@users.noreply.github.com>
2023-03-24 10:38:02 +0100

    aggiunta put shoe backend

--- howgotem_backend/src/main/java/it/alexpaganucci/howgotem/controllers/ShoeController.java
+++ howgotem_backend/src/main/java/it/alexpaganucci/howgotem/controllers/ShoeController.java
@@ -13,6 +13,7 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -30,99 +31,109 @@ public class ShoeController {
 
 	@Autowired
 	private ShoeService shoeService;
-	
+
 	@PostMapping
-    public ResponseEntity<ShoeDto> addShoe(@Valid @RequestBody ShoeDto shoeDto) {
-        Shoe shoe = new Shoe(shoeDto.getModel(), shoeDto.getSkuCode(), shoeDto.getBrand(), shoeDto.getColor(), shoeDto.getUrlImg());
-        List<Size> sizes = new ArrayList<>();
-        for (SizeDto sizeDto : shoeDto.getSizes()) {
-            Size size = new Size(sizeDto.getSize(), sizeDto.getQuantityAvailable(), sizeDto.getPrice(), shoe);
-            sizes.add(size);
-        }
-        shoe.setSizes(sizes);
-        Shoe savedShoe = shoeService.add(shoe);
+	public ResponseEntity<ShoeDto> addShoe(@Valid @RequestBody ShoeDto shoeDto) {
+		Shoe shoe = new Shoe(shoeDto.getModel(), shoeDto.getSkuCode(), shoeDto.getBrand(), shoeDto.getColor(),
+				shoeDto.getUrlImg());
+		List<Size> sizes = new ArrayList<>();
+		for (SizeDto sizeDto : shoeDto.getSizes()) {
+			Size size = new Size(sizeDto.getSize(), sizeDto.getQuantityAvailable(), sizeDto.getPrice(), shoe);
+			sizes.add(size);
+		}
+		shoe.setSizes(sizes);
+		Shoe savedShoe = shoeService.add(shoe);
 
-        ShoeDto savedShoeDto = new ShoeDto();
-        savedShoeDto.setModel(savedShoe.getModel());
-        savedShoeDto.setSkuCode(savedShoe.getSkuCode());
-        savedShoeDto.setBrand(savedShoe.getBrand());
-        savedShoeDto.setColor(savedShoe.getColor());
-        savedShoeDto.setUrlImg(savedShoe.getUrlImg());
-        List<SizeDto> savedSizeDtos = new ArrayList<>();
-        for (Size size : savedShoe.getSizes()) {
-            SizeDto sizeDto = new SizeDto();
-            sizeDto.setSize(size.getSize());
-            sizeDto.setQuantityAvailable(size.getQuantityAvailable());
-            sizeDto.setPrice(size.getPrice());
-            savedSizeDtos.add(sizeDto);
-        }
-        savedShoeDto.setSizes(savedSizeDtos);
+		ShoeDto savedShoeDto = new ShoeDto();
+		savedShoeDto.setModel(savedShoe.getModel());
+		savedShoeDto.setSkuCode(savedShoe.getSkuCode());
+		savedShoeDto.setBrand(savedShoe.getBrand());
+		savedShoeDto.setColor(savedShoe.getColor());
+		savedShoeDto.setUrlImg(savedShoe.getUrlImg());
+		List<SizeDto> savedSizeDtos = new ArrayList<>();
+		for (Size size : savedShoe.getSizes()) {
+			SizeDto sizeDto = new SizeDto();
+			sizeDto.setSize(size.getSize());
+			sizeDto.setQuantityAvailable(size.getQuantityAvailable());
+			sizeDto.setPrice(size.getPrice());
+			savedSizeDtos.add(sizeDto);
+		}
+		savedShoeDto.setSizes(savedSizeDtos);
 
-        return ResponseEntity.status(HttpStatus.CREATED).body(savedShoeDto);
-    }
-	
-	@DeleteMapping("{id}")
-	public ResponseEntity<Object> deleteShoe(@PathVariable Long id){
+		return ResponseEntity.status(HttpStatus.CREATED).body(savedShoeDto);
+	}
+
+	@DeleteMapping("/{id}")
+	public ResponseEntity<Object> deleteShoe(@PathVariable Long id) {
 		Optional<Shoe> optShoe = shoeService.findById(id);
-		
-		if( !optShoe.isPresent() )  {
+
+		if (!optShoe.isPresent()) {
 			return new ResponseEntity<>("SCARPA NON TROVATA", HttpStatus.NOT_FOUND);
 		} else {
 			Shoe s = optShoe.get();
 			shoeService.delete(s.getId());
 		}
-		return new ResponseEntity<>(
-				String.format("Scarpa con id %d cancellata!", id), HttpStatus.OK);
+		return new ResponseEntity<>(String.format("Scarpa con id %d cancellata!", id), HttpStatus.OK);
 	}
-	
+
+
+	@PutMapping("/{id}")
+	public ResponseEntity<Shoe> updateShoe(@PathVariable("id") Long shoeId, @RequestBody ShoeDto shoeDto) {
+	    Shoe updatedShoe = shoeService.updateShoeSize(shoeId, shoeDto);
+	    if (updatedShoe == null) {
+	        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
+	    }
+	    return ResponseEntity.ok(updatedShoe);
+	}
+
 	@GetMapping
-	public ResponseEntity<List<Shoe>> getAllShoe(){
+	public ResponseEntity<List<Shoe>> getAllShoe() {
 		List<Shoe> list = shoeService.findAll();
-		if( list.isEmpty() ) {
+		if (list.isEmpty()) {
 			return new ResponseEntity<>(HttpStatus.NOT_FOUND);
 		}
-		
+
 		return new ResponseEntity<>(list, HttpStatus.OK);
 	}
-	
+
 	@GetMapping("{id}")
-	public ResponseEntity<Object> findShoeById(@PathVariable("id") Long id){
-		return new ResponseEntity<>(shoeService.findById(id)
-				.orElseThrow(()-> new ShoeNotFoundException(id)), HttpStatus.OK);
+	public ResponseEntity<Object> findShoeById(@PathVariable("id") Long id) {
+		return new ResponseEntity<>(shoeService.findById(id).orElseThrow(() -> new ShoeNotFoundException(id)),
+				HttpStatus.OK);
 	}
-	
+
 	@GetMapping("/filter_by_part_of_model={m}")
-	public ResponseEntity<List<Shoe>> filterShoeByPartOfModel(@PathVariable String m){
+	public ResponseEntity<List<Shoe>> filterShoeByPartOfModel(@PathVariable String m) {
 		return new ResponseEntity<>(shoeService.filterShoeByPartOfModel(m), HttpStatus.OK);
 	}
-	
+
 	@GetMapping("/filter_by_model={m}")
-	public ResponseEntity<List<Shoe>> filterShoeByModel(@PathVariable String m){
+	public ResponseEntity<List<Shoe>> filterShoeByModel(@PathVariable String m) {
 		return new ResponseEntity<>(shoeService.filterShoeByModel(m), HttpStatus.OK);
 	}
-	
+
 	@GetMapping("/filter_by_sku_code={s}")
-	public ResponseEntity<Optional<Shoe>> filterShoeBySkuCode(@PathVariable String s){
+	public ResponseEntity<Optional<Shoe>> filterShoeBySkuCode(@PathVariable String s) {
 		return new ResponseEntity<>(shoeService.filterShoeBySkuCode(s), HttpStatus.OK);
 	}
-	
+
 	@GetMapping("/filter_by_brand={b}")
-	public ResponseEntity<List<Shoe>> filterShoeByBrand(@PathVariable String b){
+	public ResponseEntity<List<Shoe>> filterShoeByBrand(@PathVariable String b) {
 		return new ResponseEntity<>(shoeService.filterShoeByBrand(b), HttpStatus.OK);
 	}
-	
+
 	@GetMapping("/filter_by_color={c}")
-	public ResponseEntity<List<Shoe>> filterShoeByColor(@PathVariable String c){
+	public ResponseEntity<List<Shoe>> filterShoeByColor(@PathVariable String c) {
 		return new ResponseEntity<>(shoeService.filterShoeByColor(c), HttpStatus.OK);
 	}
-	
+
 	@GetMapping("/filter_by_size={s}")
-	public ResponseEntity<List<Shoe>> filterShoeBySize(@PathVariable double s){
+	public ResponseEntity<List<Shoe>> filterShoeBySize(@PathVariable double s) {
 		return new ResponseEntity<>(shoeService.filterShoeBySize(s), HttpStatus.OK);
 	}
-	
+
 	@GetMapping("/filter_by_price={p}")
-	public ResponseEntity<List<Shoe>> filterShoeByMaxPrice(@PathVariable double p){
+	public ResponseEntity<List<Shoe>> filterShoeByMaxPrice(@PathVariable double p) {
 		return new ResponseEntity<>(shoeService.filterShoeByPrice(p), HttpStatus.OK);
 	}
 }
--- howgotem_backend/src/main/java/it/alexpaganucci/howgotem/services/ShoeService.java
+++ howgotem_backend/src/main/java/it/alexpaganucci/howgotem/services/ShoeService.java
@@ -9,6 +9,9 @@ import org.springframework.stereotype.Service;
 
 import it.alexpaganucci.howgotem.entities.Shoe;
 import it.alexpaganucci.howgotem.entities.Size;
+import it.alexpaganucci.howgotem.exceptions.ShoeNotFoundException;
+import it.alexpaganucci.howgotem.payloads.ShoeDto;
+import it.alexpaganucci.howgotem.payloads.SizeDto;
 import it.alexpaganucci.howgotem.repositories.ShoeRepository;
 import it.alexpaganucci.howgotem.repositories.SizeRepository;
 
@@ -19,58 +22,99 @@ public class ShoeService {
 	private ShoeRepository shoeRepository;
 	@Autowired
 	private SizeRepository sizeRepository;
-	
+
 	public Shoe add(Shoe s) {
-	    List<Size> sizes = s.getSizes();
-	    s.setSizes(null); // rimuove le size dalla scarpa
-	    Shoe savedShoe = shoeRepository.save(s); // salva la scarpa
-	    // Aggiungi le size salvate alla scarpa
-	    List<Size> savedSizes = new ArrayList<>();
-	    for (Size size : sizes) {
-	        size.setShoe(savedShoe); // Imposta la scarpa associata alla size
-	        savedSizes.add(sizeRepository.save(size)); // Salva la size e aggiungila alla lista
-	    }
-	    savedShoe.setSizes(savedSizes); // Imposta la lista di size salvate nella scarpa
-	    return savedShoe; // Restituisce la scarpa con le size salvate
+		List<Size> sizes = s.getSizes();
+		s.setSizes(null); // rimuove le size dalla scarpa
+		Shoe savedShoe = shoeRepository.save(s); // salva la scarpa
+		// Aggiungi le size salvate alla scarpa
+		List<Size> savedSizes = new ArrayList<>();
+		for (Size size : sizes) {
+			size.setShoe(savedShoe); // Imposta la scarpa associata alla size
+			savedSizes.add(sizeRepository.save(size)); // Salva la size e aggiungila alla lista
+		}
+		savedShoe.setSizes(savedSizes); // Imposta la lista di size salvate nella scarpa
+		return savedShoe; // Restituisce la scarpa con le size salvate
 	}
-	
+
+	public Shoe updateShoeSize(Long shoeId, ShoeDto shoeDto) {
+	    Optional<Shoe> optionalShoe = shoeRepository.findById(shoeId);
+	    if (optionalShoe.isPresent()) {
+	        Shoe existingShoe = optionalShoe.get();
+	        // Update shoe details
+	        existingShoe.setModel(shoeDto.getModel());
+	        existingShoe.setSkuCode(shoeDto.getSkuCode());
+	        existingShoe.setBrand(shoeDto.getBrand());
+	        existingShoe.setColor(shoeDto.getColor());
+	        existingShoe.setUrlImg(shoeDto.getUrlImg());
+
+	        // Update shoe sizes
+	        List<SizeDto> newSizeDtos = shoeDto.getSizes();
+	        List<Size> existingSizes = existingShoe.getSizes();
+
+	        // Iterate through each new size and update or add it to the existing sizes
+	        for (SizeDto newSizeDto : newSizeDtos) {
+	            boolean sizeFound = false;
+	            for (Size existingSize : existingSizes) {
+	                if (existingSize.getSize() == newSizeDto.getSize()) {
+	                    existingSize.setQuantityAvailable(newSizeDto.getQuantityAvailable());
+	                    existingSize.setPrice(newSizeDto.getPrice());
+	                    sizeFound = true;
+	                    break;
+	                }
+	            }
+	            // If the size was not found, add a new size to the existing sizes
+	            if (!sizeFound) {
+	                Size newShoeSize = new Size(newSizeDto.getSize(), newSizeDto.getQuantityAvailable(), newSizeDto.getPrice(), existingShoe);
+	                existingSizes.add(newShoeSize);
+	                existingShoe.setSizes(existingSizes);
+	            }
+	        }
+	        Shoe updatedShoe = add(existingShoe);
+	        return updatedShoe;
+	    } else {
+	        // Throw exception if shoe does not exist
+	        throw new ShoeNotFoundException(shoeId);
+	    }
+	}
+
 	public void delete(Long id) {
 		shoeRepository.deleteById(id);
 	}
-	
+
 	public List<Shoe> findAll() {
 		return shoeRepository.findAll();
 	}
-	
+
 	public Optional<Shoe> findById(Long id) {
 		return shoeRepository.findById(id);
 	}
-	
-	public List<Shoe> filterShoeByPartOfModel(String m){
+
+	public List<Shoe> filterShoeByPartOfModel(String m) {
 		return shoeRepository.filterShoeByPartOfModel(m);
 	}
-	
-	public List<Shoe> filterShoeByModel(String m){
+
+	public List<Shoe> filterShoeByModel(String m) {
 		return shoeRepository.filterShoeByModel(m);
 	}
-	
-	public Optional<Shoe> filterShoeBySkuCode(String s){
+
+	public Optional<Shoe> filterShoeBySkuCode(String s) {
 		return shoeRepository.filterShoeBySkuCode(s);
 	}
-	
-	public List<Shoe> filterShoeByBrand(String b){
+
+	public List<Shoe> filterShoeByBrand(String b) {
 		return shoeRepository.filterShoeByBrand(b);
 	}
-	
-	public List<Shoe> filterShoeByColor(String c){
+
+	public List<Shoe> filterShoeByColor(String c) {
 		return shoeRepository.filterShoeByColor(c);
 	}
-	
-	public List<Shoe> filterShoeBySize(double s){
+
+	public List<Shoe> filterShoeBySize(double s) {
 		return shoeRepository.filterShoeBySize(s);
 	}
-	
-	public List<Shoe> filterShoeByPrice(double p){
+
+	public List<Shoe> filterShoeByPrice(double p) {
 		return shoeRepository.filterShoeByPrice(p);
 	}
 
